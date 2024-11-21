c3f4465004580e61c88cf6fabcfdd3acee1a9bbc
Martin Rios <irios@phorus.group>
2024-11-21 11:06:15 -0300

    Fix

--- codeclimate/run.sh
+++ codeclimate/run.sh
@@ -9,9 +9,7 @@ REPORT_FORMAT=$5
 ENGINE_MEMORY_LIMIT_BYTES=$6
 CODECLIMATE_DEBUG=$7
 
-echo "Running with source code: $SOURCE_CODE, and timeout $TIMEOUT_SECONDS"
-
-APP_PATH=$SOURCE_CODE
+APP_PATH="/code"
 REPORT_FILENAME_PREFIX="code-quality-report"
 REPORT_FORMAT=${REPORT_FORMAT:-json}
 DEFAULT_FILES_PATH=${DEFAULT_FILES_PATH:-/codeclimate_defaults}
@@ -26,21 +24,21 @@ ENGINE_MEMORY_LIMIT_BYTES=${ENGINE_MEMORY_LIMIT_BYTES:-1024000000} # default 1 G
 
 # Copy default config files unless already present for csslint, eslint (ignore), rubocop and coffeelint
 for config_file in .csslintrc .eslintignore .rubocop.yml coffeelint.json; do
-  if [ ! -f "$APP_PATH/$config_file" ] ; then
-    cp "$DEFAULT_FILES_PATH/$config_file" "$APP_PATH/"
+  if [ ! -f "$SOURCE_CODE/$config_file" ] ; then
+    cp -rf "$DEFAULT_FILES_PATH/$config_file" "$SOURCE_CODE/"
   fi
 done
 
 # Copy default config file unless already present for eslint
 # NB: check for all supported config files
-if ! [ -f "$APP_PATH/.eslintrc.js" -o -f "$APP_PATH/.eslintrc.yaml" -o -f "$APP_PATH/.eslintrc.yml" -o -f "$APP_PATH/.eslintrc.json" -o -f "$APP_PATH/.eslintrc" ] ; then
-  cp "$DEFAULT_FILES_PATH/.eslintrc.yml" "$APP_PATH/"
+  if ! [ -f "$SOURCE_CODE/.eslintrc.js" ] || [ -f "$SOURCE_CODE/.eslintrc.yaml" ] || [ -f "$SOURCE_CODE/.eslintrc.yml" ] || [ -f "$SOURCE_CODE/.eslintrc.json" ] || [ -f "$SOURCE_CODE/.eslintrc" ] ; then
+  cp -rf "$DEFAULT_FILES_PATH/.eslintrc.yml" "$SOURCE_CODE/"
 fi
 
 # Detect eslint version for using proper channel
 ESLINT_CHANNEL="stable"
-if [ -f "$APP_PATH/package.json" ] ; then
-  ESLINT_VERSION_FROM_PACKAGE_JSON=$(jq -r '[.dependencies.eslint, .devDependencies.eslint] | map(select (. != null)) | first' "$APP_PATH/package.json")
+if [ -f "$SOURCE_CODE/package.json" ] ; then
+  ESLINT_VERSION_FROM_PACKAGE_JSON=$(jq -r '[.dependencies.eslint, .devDependencies.eslint] | map(select (. != null)) | first' "$SOURCE_CODE/package.json")
   # Supported notation: ~5.3.0, ^5.3.0, 5.3.0
   ESLINT_MAJOR_VERSION=$(echo "$ESLINT_VERSION_FROM_PACKAGE_JSON" | sed -E 's/^[~^]?([0-9]+).*/\1/')
 
@@ -53,8 +51,8 @@ fi
 
 # Render default config file unless already present for code climate
 # NB: check for all supported config files
-if ! [ -f  "$APP_PATH/.codeclimate.yml" -o -f "$APP_PATH/.codeclimate.json" ] ; then
-  sed -e "s/__ESLINT_CHANNEL__/\"$ESLINT_CHANNEL\"/" "$DEFAULT_FILES_PATH/.codeclimate.yml.template" > "$APP_PATH/.codeclimate.yml"
+if ! [ -f  "$SOURCE_CODE/.codeclimate.yml" ] || [ -f "$SOURCE_CODE/.codeclimate.json" ] ; then
+  sed -e "s/__ESLINT_CHANNEL__/\"$ESLINT_CHANNEL\"/" "$DEFAULT_FILES_PATH/.codeclimate.yml.template" > "$SOURCE_CODE/.codeclimate.yml"
 fi
 
 # Pull the code climate image in advance of running the container to
@@ -75,7 +73,7 @@ docker run --rm \
     "${CODECLIMATE_FULL_IMAGE}" --no-check-version engines:install > /dev/null
 
 if [ $? -ne 0 ]; then
-    echo "Could not install code climate engines for the repository at $APP_PATH"
+    echo "Could not install code climate engines for the repository at $SOURCE_CODE"
     exit 1
 fi
 
@@ -101,13 +99,13 @@ if echo "$REPORT_FORMAT" | grep -Eq '(json|html)' ; then
       "${CODECLIMATE_FULL_IMAGE}" --no-check-version analyze ${CODECLIMATE_DEV:+--dev} -f "$REPORT_FORMAT" > "/tmp/raw_codeclimate.$REPORT_FORMAT"
 
   if [ $? -ne 0 ]; then
-      echo "Could not analyze code quality for the repository at $APP_PATH"
+      echo "Could not analyze code quality for the repository at $SOURCE_CODE"
       exit 1
   fi
 
   # redirect STDOUT to disk (default), unless REPORT_STDOUT is set
   if [ -z "$REPORT_STDOUT" ]; then
-    exec > "$APP_PATH/$REPORT_FILENAME_PREFIX.$REPORT_FORMAT"
+    exec > "$SOURCE_CODE/$REPORT_FILENAME_PREFIX.$REPORT_FORMAT"
   fi
 
   if [ "$REPORT_FORMAT" = "json" ]; then
